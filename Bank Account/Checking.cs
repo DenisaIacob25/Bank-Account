c6b348de86f792145f1fa42daf3f209877a6ac94
Denisa Iacob <denisaiacob18@gmail.com>
2017-01-26 16:01:18 -0500

    inital commit

---
+++ Bank Account/Checking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Account
+{
+    class Checking : Account
+    {
+        //fields for checking
+        protected int runningBalance;
+
+    }
+}
